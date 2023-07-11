e4355e4987ec2404676aa27264886ba9f5563aeb
quachuoi-dev <85794491+quachuoi-dev@users.noreply.github.com>
2023-07-11 11:11:25 +0700

    ok

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,27 +1,61 @@
 <template>
   <div>
-    <input type="text" placeholder="Input link here" v-model="name"> |
-    <button @click="getLink()">Generate shortened link</button>
+    <input type="text" placeholder="Input link here" v-model="linkInput"> |
+    <button @click="getLink()" type="submit">Generate shortened link</button>
+    <br>
+    <h1> shorten url: </h1>
+    <a :href="linkOutput.short_url" target="_blank">{{ linkOutput.short_url }}</a>
     <hr>
-    <h1>{{ name }}</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
   data() {
     return {
-      name: ''
+      linkInput: '',
+      linkOutput: '',
     }
   },
   methods: {
-    getLink() {
-      return
+    async getLink() {
+
+      const url = new URL(
+        "https://t.ly/api/v1/link/shorten"
+      );
+
+      const headers = {
+        "Authorization": "Bearer UMs01GFU1vhLFcBBFVTFxwowKAXAFv3UuLj1KA0sZtQsjNg12Iqa3SPiBKrY",
+        "Content-Type": "application/json",
+        "Accept": "*/*",
+      };
+
+      let body = {
+        "long_url": this.linkInput,
+        "domain": "https://t.ly/",
+        "expire_at_datetime": "2035-01-17 15:00:00",
+        "description": '',
+        "public_stats": true,
+      };
+
+      this.linkOutput = await fetch(url, {
+        method: "POST",
+        headers,
+        body: JSON.stringify(body),
+      }).then(
+        async (response) => {
+          if (!response.ok) {
+            const msg = await response.json()
+            this.linkOutput = new Error(msg.message)
+            return this.linkOutput
+          }
+          return await response.json()
+        }
+      )
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped></style>
